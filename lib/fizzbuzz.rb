3642dec5146add26e924e192aa363fa78079da2b
Wardah Tarar <ec2-user@ip-172-31-4-54.ec2.internal>
2019-02-26 16:05:57 +0000

    Added full code to be tested

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,12 @@
 def fizzbuzz(number)
-    "fizz"
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
 end
 
